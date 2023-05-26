97f191e04a4f9631765a4e1fc5b4dc38e6d942e3
Perfecterudite <ayomideadedeji99@yahoo.com>
2023-05-26 10:13:58 -0600

    First commit

---
+++ src/Company/loginPage.java
@@ -0,0 +1,18 @@
+package Company;
+
+import javafx.fxml.Initializable;
+import javafx.*;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
+import javafx.fxml.FXML;
+
+public class loginPage {
+    @FXML
+    private TextField username;
+    @FXML private TextField password;
+    @FXML private Label locationName;
+
+    public void loginClick(){
+
+    }
+}
