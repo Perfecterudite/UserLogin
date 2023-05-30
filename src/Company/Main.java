3d6b6117a6a25bead0baef6df986c360ae02be23
Perfecterudite <ayomideadedeji99@yahoo.com>
2023-05-30 07:47:33 -0600

    Setting up database connection

--- src/Company/Main.java
+++ src/Company/Main.java
@@ -18,6 +18,8 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
+        DatabaseConn.startConnection();
         launch(args);
+        DatabaseConn.endConnection();
     }
 }
--- src/Company/loginPage.java
+++ src/Company/loginPage.java
@@ -1,17 +1,47 @@
 package Company;
 
-import javafx.fxml.Initializable;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+
+import java.util.Optional;
+import javafx.scene.Scene;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+
 import javafx.scene.control.Label;
 import javafx.fxml.FXML;
 
-public class loginPage {
-    @FXML
-    private TextField username;
-    @FXML private TextField password;
-    @FXML private Label locationName;
+public abstract class loginPage implements Initializable{
 
-    public void loginClick(){
+    @FXML private TextField username;
+    @FXML private PasswordField password;
+    @FXML private Label locationName;
+    @FXML private Button loginBtn;
+
+    public void Initialize(URL Location, ResourceBundle resourceBundle){
+
+    }
+    public void loginClick(ActionEvent actionEvent){
+
+        String inputUsername = username.getText();
+        String inputPassword = password.getText();
 
     }
 }
