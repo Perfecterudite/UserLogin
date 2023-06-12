2bd689087d185c9acc749fe8021ff8439c6207a9
Perfecterudite <ayomideadedeji99@yahoo.com>
2023-06-12 10:17:45 -0600

    models reports testing

--- src/Company/Main.java
+++ src/Company/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("View/reports.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("View/main.fxml"));
         primaryStage.setTitle("Fortnight Scheduler");
         primaryStage.setScene(new Scene(root, 800, 575));
         primaryStage.show();
